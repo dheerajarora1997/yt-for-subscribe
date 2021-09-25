67fc88af66d5c7654d9a017d0222e0c5255fa210
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2021-09-25 17:50:03 +0530

    changes

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,26 +1,41 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
-
-
-function openNav(){
+function openNav() {
     $(".wrapper").toggleClass("active");
 }
-
-
 
 $(document).ready(function () {
     $(".sidebar ul.nav li:first-child, .sidebar-bg").click(function () {
         $(".sidebar ul.nav li:first-child i").toggleClass("fi-long-arrow-left fi-navigation");
         openNav();
     });
-
     function mobileNav() {
         $(".mob-sidebar-bg").toggleClass("open");
     }
-
     $(".mob-sidebar, .mob-nav, .close-nav").click(function () {
         mobileNav();
     });
+});
 
+// Iframe Code
+let firstVideo = '0Ll5DofUmgE';
+let my_array = ['aRvFOku7w5I', '2HNtLfetnNg', 'nyKNwwiQYcM', 'ExQe3fXj7t8', 'i0KO9Js4_dE', 'ygqbfJOdtmA', 'gU9KRJYtg5E', 'sEL5l1dATJw', 'm8N7onAJexU'];
+let currentIndex = 1;
+
+$('#getNextVideo').click(function () {
+    $('iframe').attr('src', 'https://www.youtube.com/embed/' + my_array[currentIndex]);
+    if (currentIndex == my_array.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+});
+
+$('#coin-campaign').on('shown.bs.modal', function(){
+    $('iframe').attr('src', 'https://www.youtube.com/embed/' + firstVideo);
+});
+
+$('#coin-campaign').on('hidden.bs.modal', function(){
+    currentIndex = 0;
 });
